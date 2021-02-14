ab7311f6cede05cb0cfb979245eaaee848a8231f
Nerphist <nikolailgv@ukr.net>
2021-02-14 18:12:00 +0200

    fastapi microservice added

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker volume create --name=db-volume
+docker volume create --name=db-auth-volume
+docker volume create --name=db-metrics-volume
+docker-compose build
 docker-compose up
