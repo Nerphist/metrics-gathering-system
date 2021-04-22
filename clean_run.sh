b62dd1ec38b651620e57796bbf69844dbad6c975
Nerphist <nikolailgv@ukr.net>
2021-04-22 21:48:45 +0300

    fixes

---
+++ clean_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker-compose down
+
+docker volume rm db-auth-volume
+docker volume rm db-metrics-volume
+docker volume rm db-tasks-volume
+
+docker volume create --name=db-auth-volume
+docker volume create --name=db-metrics-volume
+docker volume create --name=db-tasks-volume
+
+docker-compose build
+docker-compose up
