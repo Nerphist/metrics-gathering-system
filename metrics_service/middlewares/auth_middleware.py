3900ac1e39dac4a9dc5936ffd4a5daf9d3f4bcd7
Nerphist <nikolailgv@ukr.net>
2021-04-25 22:23:13 +0300

    server api key

--- metrics_service/middlewares/auth_middleware.py
+++ metrics_service/middlewares/auth_middleware.py
@@ -1,14 +1,22 @@
+import os
+
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
 
 from auth_api import auth_user
 
+SERVER_API_KEY = os.environ.get('SERVER_API_KEY', '123')
+
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        if not auth_user(dict(request.headers)):
-            response = JSONResponse(content={'detail': 'Authorization Error'})
-        else:
-            response = await call_next(request)
-        return response
+        headers = dict(request.headers)
+
+        if headers.get('server-api-key') == SERVER_API_KEY:
+            return await call_next(request)
+
+        if auth_user(headers):
+            return await call_next(request)
+
+        return JSONResponse(content={'detail': 'Authorization Error'})
