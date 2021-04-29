99e7440f4d5c0d617c5a91738cea0ebd06e1ab4a
Nerphist <nikolailgv@ukr.net>
2021-04-29 17:20:18 +0300

    fix

--- document_service/auth_api.py
+++ document_service/auth_api.py
@@ -11,7 +11,7 @@ SERVER_API_KEY = os.environ.get('SERVER_API_KEY', '123')
 
 
 def auth_user(headers: Dict[str, str]) -> bool:
-    if os.environ.get('DEBUG'):
+    if os.environ.get('DEBUG') == 'True':
         return True
     response = requests.get(url=f'{AUTH_API_URL}/auth-user/',
                             headers={k.capitalize(): v for k, v in headers.items()})
@@ -19,7 +19,7 @@ def auth_user(headers: Dict[str, str]) -> bool:
 
 
 def is_admin(headers: Dict[str, str]) -> bool:
-    if os.environ.get('DEBUG'):
+    if os.environ.get('DEBUG') == 'True':
         return True
     response = requests.get(url=f'{AUTH_API_URL}/auth-user/',
                             headers={k.capitalize(): v for k, v in headers.items()})
