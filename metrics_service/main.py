3f4f52f0d0159b4a3a45ad5246a29aa37c265784
Nerphist <nikolailgv@ukr.net>
2021-05-01 17:47:14 +0300

    fix

--- metrics_service/main.py
+++ metrics_service/main.py
@@ -1,7 +1,6 @@
-import os
-
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from middlewares.auth_middleware import AuthMiddleware
 from routes import *
@@ -9,6 +8,17 @@ from routes import *
 app = FastAPI()
 app.include_router(metrics_router)
 app.add_middleware(AuthMiddleware)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8002, log_level="info")
