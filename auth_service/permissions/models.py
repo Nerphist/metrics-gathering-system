049ab871912d31497ad753f0f404dcfd27002eeb
Nerphist <nikolailgv@ukr.net>
2021-05-12 13:48:49 +0300

    permissions per entity removed

--- auth_service/permissions/models.py
+++ auth_service/permissions/models.py
@@ -8,26 +8,4 @@ from utils import AbstractCreateUpdateModel
 
 
 class PermissionGroup(AbstractCreateUpdateModel):
-    class Meta:
-        index_together = [
-            ("user_group_id", "entity_type", "entity_id"),
-        ]
-
-    class Action(Enum):
-        create = 1
-        read = 2
-        update = 3
-        delete = 4
-
-    class EntityTypes(Enum):
-        # location_group = 1
-        building = 2
-        floor = 3
-        room = 4
-        device = 5
-
-    user_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE, null=False, related_name='permission_groups',
-                                   db_index=True)
-    entity_type = models.CharField(max_length=255, null=False, db_index=True)
-    entity_id = models.IntegerField(null=False)
-    permission_set = models.JSONField(default=list)
+    pass
--- auth_service/permissions/urls.py
+++ auth_service/permissions/urls.py
@@ -3,5 +3,4 @@ from django.urls import path
 from permissions.views import *
 
 urlpatterns = [
-    path('', PermissionsListView.as_view(), name='Get all user permissions'),
 ]
